4e9141d159cd182f9ee39b7c5cb6e767fd89c0b0
mouad <mouadzaza@gmail.com>
2019-04-09 00:42:10 +0100

    v1.3 with atom + virtualbox 6.0 + vagrant 2.2.4 + ansible 2.7.9 + chefdk 3.8.14

--- vvaac.sh
+++ vvaac.sh
@@ -5,48 +5,45 @@
 #remove virtual-box if installed
 
 sudo apt install software-properties-common apt-transport-https wget curl
-
-# sudo apt remove virtualbox virtualbox-5.2
 #
-# #installation virtual-box
-# sudo apt update
-# sudo apt-get install gcc make linux-headers-$(uname -r) dkms
+sudo apt remove virtualbox virtualbox-5.2
 #
-# wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-# wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+# installation virtual-box
 #
-# sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian bionic contrib" >> /etc/apt/sources.list.d/virtualbox.list'
+sudo apt update
+sudo apt-get install gcc make linux-headers-$(uname -r) dkms
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian bionic contrib" >> /etc/apt/sources.list.d/virtualbox.list'
+sudo apt update
+sudo apt-get install virtualbox-6.0
 #
-#
-# sudo apt update
-# sudo apt-get install virtualbox-6.0
 #installation vagrant
+#
 wget --directory-prefix=/var https://releases.hashicorp.com/vagrant/2.2.4/vagrant_2.2.4_x86_64.deb
+#
 sudo dpkg -i  --force-all /var/vagrant_2.2.4_x86_64.deb
-
-
+#
 #installation ansible
 #
-# sudo apt install software-properties-common apt-transport-https wget curl
-#
-# sudo apt-add-repository ppa:ansible/ansible
-# sudo apt update
-# sudo apt install ansible
+sudo apt install software-properties-common apt-transport-https -y
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt update -y
+sudo apt install ansible -y
 #
 # #installation atom
 #
-# #wget -q https://packagecloud.io/AtomEditor/atom/gpgkey -O- | sudo apt-key add -
-# #sudo add-apt-repositoh/varamd64] https://packagecloud.io/AtomEditor/atom/any/ any main"
-# snap install atom --classic
+snap install atom --classic
+#
 # #installation chefdk
 #
+curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P chefdk -c stable
 #
-# curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P chefdk -c stable
-#
-#
-#
+#install gem
+wget --directory-prefix=/var https://makandracards.com/makandra/716-install-rubygems-on-ubuntu-debian/attachments/151
+sudo dpkg -i  --force-all /var/rubygems_1.8.7-1_amd64.deb
+chef gem  install kitchen-vagrant -y
+chef gem  install kitchen-ansible -y
 # #update and upgrade
-# sudo apt update
-# sudo apt upgrade
-#
-# #mouad
+sudo apt update -y
+sudo apt upgrade -y
